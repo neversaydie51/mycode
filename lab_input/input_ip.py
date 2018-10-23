2022a13f3baf79764ac1821ee75ba3938936cce4
James DuDasko <neversaydie51@gmail.com>
2018-10-23 08:41:02 -0400

    new practice code

---
+++ lab_input/input_ip.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+#Author: James DuDasko
+#Collect and display IP addreses
+
+#Collect IP addresses from the user.
+user_input = input('Please enter an IPv4 IP address:')
+
+#Print the input from user.
+print("you told me the IPv4 address is:" + user_input)
