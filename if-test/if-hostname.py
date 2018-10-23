c7a96f8154ce600e93ab6ba6985448a78c2af3ff
James DuDasko <neversaydie51@gmail.com>
2018-10-23 13:49:06 -0400

    Loop Practice 1

---
+++ if-test/if-hostname.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+#Author: James DuDasko
+
+#variables
+hostname = input('What is the systems hostname? ')
+hostname = hostname.upper()
+
+#if loop
+if hostname == 'MTG':
+    print('hostname matches expected config')
+    print('The hostname was found to be mtg')
+
+#end loop
+print('Exiting the script')
