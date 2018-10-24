0f554bef9b9ad91e97b69cfabdb165187e216f81
James DuDasko <neversaydie51@gmail.com>
2018-10-24 13:11:09 -0400

    For Loop Testing

---
+++ introfor/forloop1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+#Author: James DuDasko
+
+vendors = ['cisco', 'juniper', 'big_ip', 'f5', 'arista']
+
+#For lop
+for x in vendors:
+    print( "The vendor is:" + x)
+print("\nOur loop has ended." )
