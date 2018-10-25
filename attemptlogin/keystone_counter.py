84190403c4dbf8857be16bcd3e507aaa7ad90073
James DuDasko <neversaydie51@gmail.com>
2018-10-25 09:05:24 -0400

    Added Success Loop

--- attemptlogin/keystone_counter.py
+++ attemptlogin/keystone_counter.py
@@ -2,6 +2,7 @@
 #Author: James DuDasko
 #parse keystone.common.wsgi and return failed attempts
 loginfail = 0
+loginsuccess = 0
 keystone_file = open('/home/student/mycode/attemptlogin/keystone.common.wsgi', 'r')
 
 keystone_file_lines = keystone_file.readlines()
@@ -11,3 +12,10 @@ for i in range(len(keystone_file_lines)):
     if "- - - - -] Authorization failed" in keystone_file_lines[i]:
         loginfail += 1 # this is the same as loginfail = loginfail + 1
 print('The number of failed log in attempts is ' + str(loginfail))
+
+#Loop will search for successful logins
+for i in range(len(keystone_file_lines)):
+    if "GET http://controller:5000/v3/users/63dee1ed626b4040bcd43b3492997a8c/projects" in keystone_file_lines[i]:
+        loginsuccess += 1
+print('The number of successful log in attempts is ' + str(loginsuccess))
+
